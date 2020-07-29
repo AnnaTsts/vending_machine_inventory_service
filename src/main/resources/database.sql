7c1fa000cae6d66de28a9646c7faa53efe1856ae
annatsytsyluik <ann.tsytsyluik@gmail.com>
2020-07-29 16:49:14 +0300

    sql file for db creation was changed.

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,3 +1,15 @@
+alter table event drop constraint product_in_vent_ref;
+
+alter table event drop constraint shelf_in_event;
+
+alter table event drop constraint event_type_ref;
+
+alter table product drop constraint brand_ref;
+
+alter table product drop constraint product_type_ref;
+
+alter table store drop constraint product_to_shelf_ref;
+
 drop table brand;
 
 drop table event;
@@ -14,7 +26,7 @@ drop table store;
 /* Table: brand                                                 */
 /*==============================================================*/
 create table brand (
-   brand_id             INT4                 not null,
+   brand_id             IDENTITY                 not null,
    brand_name           VARCHAR(128)         not null,
    constraint PK_BRAND primary key (brand_id)
 );
@@ -23,12 +35,12 @@ create table brand (
 /* Table: event                                                 */
 /*==============================================================*/
 create table event (
-   event_id             INT4                 not null,
-   product_id           INT4                 null,
-   shelf_id             INT4                 null,
-   event_type_id        INT4                 null,
+   event_id             IDENTITY                 not null,
+   product_id           BIGINT                 null,
+   shelf_id             BIGINT                 null,
+   event_type_id        BIGINT                 null,
    event_time           DATE                 null,
-   quantity             INT4                 null,
+   quantity             BIGINT                 null,
    constraint PK_EVENT primary key (event_id)
 );
 
@@ -36,7 +48,7 @@ create table event (
 /* Table: event_type                                            */
 /*==============================================================*/
 create table event_type (
-   event_type_id        INT4                 not null,
+   event_type_id        IDENTITY                 not null,
    evet_type_name       VARCHAR(128)         not null,
    constraint PK_EVENT_TYPE primary key (event_type_id)
 );
@@ -45,9 +57,9 @@ create table event_type (
 /* Table: product                                               */
 /*==============================================================*/
 create table product (
-   product_id           INT4                 not null,
-   brand_id             INT4                 not null,
-   product_type_id      INT4                 not null,
+   product_id           IDENTITY                 not null,
+   brand_id             BIGINT                 not null,
+   product_type_id      BIGINT                 not null,
    product_name         VARCHAR(128)         not null,
    weight               DECIMAL              null,
    price                DECIMAL              not null,
@@ -58,7 +70,7 @@ create table product (
 /* Table: product_type                                          */
 /*==============================================================*/
 create table product_type (
-   product_type_id      INT4                 not null,
+   product_type_id      IDENTITY                 not null,
    product_type_name    VARCHAR(128)         not null,
    constraint PK_PRODUCT_TYPE primary key (product_type_id)
 );
@@ -67,10 +79,10 @@ create table product_type (
 /* Table: store                                                 */
 /*==============================================================*/
 create table store (
-   shelf_id             INT4                 not null,
-   product_id           INT4                 null,
+   shelf_id             IDENTITY                 not null,
+   product_id           BIGINT                 null,
    shelf_display_name   VARCHAR(16)          not null,
-   quantity             INT4                 null,
+   quantity             BIGINT                 null,
    constraint PK_STORE primary key (shelf_id)
 );
 
@@ -102,3 +114,14 @@ alter table product
 alter table store
    add constraint product_to_shelf_ref foreign key (product_id)
       references product (product_id)
+
+Insert into product_type (product_type_name) values ('water');
+Insert into product_type (product_type_name) values ('tea');
+Insert into product_type (product_type_name) values ('coke');
+Insert into product_type (product_type_name) values ('pop');
+
+INSERT into BRAND (BRAND_NAME) values ( 'Coca cola' );
+
+insert into PRODUCT (brand_id, product_type_id, product_name, weight, price) values ( 1,3,'cola',0.5,10.0 )
+
+insert into store(product_id,shelf_display_name,quantity) values
